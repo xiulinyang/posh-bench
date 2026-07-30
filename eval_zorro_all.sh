8ed1f5bb270bb8c80a78758bffeb974c6e930971
xiulinyang <xiulin.yang.compling@gmail.com>
2026-07-30 16:40:10 -0400

    eval posh

--- eval_zorro_all.sh
+++ eval_zorro_all.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-python benchmark_eval.py xiulinyang/linear_only_0 --eval_dataset posh
-python benchmark_eval.py xiulinyang/linear_only_128 --eval_dataset posh
-python benchmark_eval.py xiulinyang/linear_only_96 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/linear_only_0 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/linear_only_128 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/linear_only_96 --eval_dataset posh
+#
+#python benchmark_eval.py xiulinyang/dynamic_only_96 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/dynamic_only_128 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/dynamic_only_0 --eval_dataset posh
+#
+#python benchmark_eval.py xiulinyang/linear_dyck_96 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/linear_dyck_128 --eval_dataset posh
+#python benchmark_eval.py xiulinyang/linear_dyck_0 --eval_dataset posh
 
-python benchmark_eval.py xiulinyang/dynamic_only_96 --eval_dataset posh
-python benchmark_eval.py xiulinyang/dynamic_only_128 --eval_dataset posh
-python benchmark_eval.py xiulinyang/dynamic_only_0 --eval_dataset posh
-
-python benchmark_eval.py xiulinyang/linear_dyck_96 --eval_dataset posh
-python benchmark_eval.py xiulinyang/linear_dyck_128 --eval_dataset posh
-python benchmark_eval.py xiulinyang/linear_dyck_0 --eval_dataset posh
-
-python benchmark_eval.py xiulinyang/dynamic_dyck_96 --eval_dataset posh
-python benchmark_eval.py xiulinyang/dynamic_dyck_128 --eval_dataset posh
-python benchmark_eval.py xiulinyang/dynamic_dyck_0 --eval_dataset posh
+python benchmark_eval.py xiulinyang/dynamic_dyck_96 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/dynamic_dyck_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/dynamic_dyck_0 --eval_dataset zorro
 
 
-python benchmark_eval.py xiulinyang/TPT_10Mf_128 --eval_dataset posh
-python benchmark_eval.py xiulinyang/TPT_10Mf_96 --eval_dataset posh
-python benchmark_eval.py xiulinyang/TPT_10Mf_0 --eval_dataset posh
+python benchmark_eval.py xiulinyang/TPT_10Mf_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/TPT_10Mf_96 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/TPT_10Mf_0 --eval_dataset zorro
 
 
