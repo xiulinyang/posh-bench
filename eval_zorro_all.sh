7ef48a64de0bc203e7b04cc81ce0e5a6926eed31
xiulinyang <xiulin.yang.compling@gmail.com>
2026-07-30 16:06:44 -0400

    update zorro eval

---
+++ eval_zorro_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+python benchmark_eval.py xiulinyang/linear_only_0 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_only_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_only_96 --eval_dataset zorro
+
+python benchmark_eval.py xiulinyang/dynamic_only_96 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/dynamic_only_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/dynamic_only_0 --eval_dataset zorro
+
+python benchmark_eval.py xiulinyang/linear_dyck_96 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_dyck_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_dyck_0 --eval_dataset zorro
+
+python benchmark_eval.py xiulinyang/linear_only_96 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_only_128 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_only_0 --eval_dataset zorro
